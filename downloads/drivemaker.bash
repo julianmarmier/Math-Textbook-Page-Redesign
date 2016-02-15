8d01cdbae734de8b207aa04ec3ea6947313f5b88
ETCG <ETCG@users.noreply.github.com>
2016-02-15 16:39:07 -0500

    Updated to version 2

--- downloads/drivemaker.bash
+++ downloads/drivemaker.bash
@@ -11,7 +11,6 @@
 
 #Variables:
 ISOFILE="file.iso"
-DOWNLOADLINK="http://mirrors.mit.edu/ubuntu-releases/14.04.3/ubuntu-14.04.3-desktop-amd64.iso"
 IMGFILE="file.img.dmg"
 DIR="/tmp/etcg_drivemaker"
 DISK="/dev/" #Will be updated later
@@ -22,9 +21,37 @@ echo "Setting Up..."
 sudo rm -r "$DIR" > /dev/null 2>&1 #Cleans up any old operations, to prevent any errors, and hides output
 sudo mkdir "$DIR"
 cd "$DIR"
-echo "Downloading The File..."
-sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
-echo "Converting The File To Bootable Format..."
+
+echo "OS Choices:"
+echo "  (A) Ubuntu 14.04.3 (64-bit)"
+echo "  (B) Debian 8.3.0   (64-bit, internet installer)"
+echo "  (C) Other          (You'll have to manually enter a URL)"
+echo "  (D) Skip this step (You'll have to provide a path to the downloaded ISO.)"
+echo "Please enter the letter corresponding with your choice (A, B, C, or D):"
+read userOption
+
+if [ "$userOption" == "A" ]; then
+	DOWNLOADLINK="http://mirrors.mit.edu/ubuntu-releases/14.04.3/ubuntu-14.04.3-desktop-amd64.iso"
+	echo "Downloading Ubuntu..."
+	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+elif [ "$userOption" == "B" ]; then
+	DOWNLOADLINK="caesar.acc.umu.se/debian-cd/8.3.0/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso"
+	echo "Downloading Debian..."
+	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+elif [ "$userOption" == "C" ]; then
+	echo "Please enter the ISO's URL, including the .iso:"
+	read DOWNLOADLINK
+	echo "Downloading the OS..."
+	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+else
+	echo "Please enter the ISO file's path (You can drag and drop the file here too):"
+	read ISOPATH
+	echo "Working..."
+	cp "$ISOPATH" .
+	ISOFILE="$(basename $ISOPATH)"	
+fi
+
+echo "Converting the ISO to the needed format..."
 sudo hdiutil convert -format UDRW -o "$IMGFILE" "$ISOFILE"
 echo "Please enter your drive's label (open Disk Utility, select your drive, and locate the value right of \"Device:\"):"
 read LABEL
