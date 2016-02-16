ee711b37b3760e066f4bd89ea0841cd676a5ccc5
ETCG <epictincangames@gmail.com>
2016-02-16 14:39:48 -0500

    Updated drivemaker.bash to v3

--- downloads/drivemaker.bash
+++ downloads/drivemaker.bash
@@ -15,13 +15,27 @@ IMGFILE="file.img.dmg"
 DIR="/tmp/etcg_drivemaker"
 DISK="/dev/" #Will be updated later
 
+#Functions:
+function downloadFile {
+	sudo curl "$1" --output "$ISOFILE"
+	if [ $? -eq 0 ]
+	then
+		echo "Download Successful."
+	else
+		echo "Download Failed."
+		echo "Quiting...."
+		exit 1
+	fi
+}
 #Main:
+echo
 echo "To stop this script at any time, hit Control+C"
 echo "Setting Up..."
 sudo rm -r "$DIR" > /dev/null 2>&1 #Cleans up any old operations, to prevent any errors, and hides output
 sudo mkdir "$DIR"
 cd "$DIR"
 
+echo
 echo "OS Choices:"
 echo "  (A) Ubuntu 14.04.3 (64-bit)"
 echo "  (B) Debian 8.3.0   (64-bit, internet installer)"
@@ -30,19 +44,27 @@ echo "  (D) Skip this step (You'll have to provide a path to the downloaded ISO.
 echo "Please enter the letter corresponding with your choice (A, B, C, or D):"
 read userOption
 
+echo
 if [ "$userOption" == "A" ]; then
-	DOWNLOADLINK="http://mirrors.mit.edu/ubuntu-releases/14.04.3/ubuntu-14.04.3-desktop-amd64.iso"
 	echo "Downloading Ubuntu..."
-	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+	if ! downloadFile "http://mirrors.mit.edu/ubuntu-releases/14.04.3/ubuntu-14.04.3-desktop-amd64.iso"
+	then
+		exit 1
+	fi
 elif [ "$userOption" == "B" ]; then
-	DOWNLOADLINK="caesar.acc.umu.se/debian-cd/8.3.0/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso"
 	echo "Downloading Debian..."
-	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+	if ! downloadFile "caesar.acc.umu.se/debian-cd/8.3.0/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso"
+	then
+		exit 1
+	fi
 elif [ "$userOption" == "C" ]; then
 	echo "Please enter the ISO's URL, including the .iso:"
 	read DOWNLOADLINK
 	echo "Downloading the OS..."
-	sudo curl "$DOWNLOADLINK" --output "$ISOFILE"
+	if ! downloadFile "$DOWNLOADLINK"
+	then
+		exit 1
+	fi
 else
 	echo "Please enter the ISO file's path (You can drag and drop the file here too):"
 	read ISOPATH
@@ -51,20 +73,50 @@ else
 	ISOFILE="$(basename $ISOPATH)"	
 fi
 
+echo
 echo "Converting the ISO to the needed format..."
-sudo hdiutil convert -format UDRW -o "$IMGFILE" "$ISOFILE"
+if ! sudo hdiutil convert -format UDRW -o "$IMGFILE" "$ISOFILE"
+then
+	echo "Conversion Failed."
+	echo "Quiting..."
+	exit 2
+else echo "Conversion Successful."
+fi
+
+echo
 echo "Please enter your drive's label (open Disk Utility, select your drive, and locate the value right of \"Device:\"):"
 read LABEL
 DISK+=$LABEL
+
+echo
 echo "Unmounting Disk..."
-diskutil unmountDisk "$DISK"
+if ! diskutil unmountDisk "$DISK"
+then
+	echo "Unmounting Failed."
+	echo "Quiting..."
+	exit 3
+fi
+
+echo
 echo "About to make the bootable drive, this will erase all data on the drive"
-echo "You have 20 seconds to abort...(abort by hitting \"Control\" and \"C\" at the same time)"
+echo "You have 20 seconds to abort (abort by hitting \"Control\" and \"C\" at the same time)..."
 sleep 20
-echo "Making Bootable Drive...(this will take a long time)"
-sudo dd if="$IMGFILE" of="$DISK" bs=1m
-echo "Drive Created!"
+
+echo
+echo "Making Bootable Drive (this will take a long time)..."
+if sudo dd if="$IMGFILE" of="$DISK" bs=4m
+then
+	echo "+--------------+"
+	echo "|Drive Created!|"
+	echo "+--------------+"
+	EXITSTATUS=0
+else
+	echo "+----------------+"
+	echo "|Creation Failed!|"
+	echo "+----------------+"
+	EXITSTATUS=4
+fi
 echo "Cleaning Up..."
 sudo rm -r "$DIR"
-echo "Done! Quiting..."
-exit 0
+echo "Quiting..."
+exit "$EXITSTATUS"
