66302887df1b10f69b71b6ee8032930c76466853
Gregory Conrad <GregoryConrad@users.noreply.github.com>
2017-06-15 11:05:38 -0400

    Create drivemaker.bash

--- downloads/drivemaker.bash
+++ downloads/drivemaker.bash
@@ -53,8 +53,8 @@ if [ "$userOption" == "A" ]; then
 	fi
 elif [ "$userOption" == "B" ]; then
 	echo "Downloading Debian..."
-	DEBIAN_VERSION="8.7.1"
-	if ! downloadFile "http://saimei.acc.umu.se/debian-cd/"$DEBIAN_VERSION"/amd64/iso-cd/debian-"$DEBIAN_VERSION"-amd64-netinst.iso"
+	DEBIAN_VERSION="8.8.0"
+	if ! downloadFile "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-"$DEBIAN_VERSION"-amd64-netinst.iso"
 	then
 		exit 1
 	fi
